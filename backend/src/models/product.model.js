7fba5aadda5d9ac62dd603d952b1a42d5dff9b8c
SultanaJahan25 <coursesellerbd25@gmail.com>
2026-06-30 12:15:59 +0600

    Create Product model

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Product name is required'],
+      trim: true,
+      maxlength: 150,
+    },
+
+    price: {
+      type: Number,
+      required: [true, 'Price is required'],
+      min: 0,
+    },
+
+    stock: {
+      type: Number,
+      required: [true, 'Stock is required'],
+      min: 0,
+      default: 0,
+    },
+
+    images: [
+      {
+        type: String,
+      },
+    ],
+
+    category: {
+      type: String,
+      required: [true, 'Category is required'],
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+      trim: true,
+      maxlength: 2000,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
